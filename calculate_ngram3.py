67c7fab14bd669bf488ef400b7a3d651e601da9a
FourierYe <939647181@qq.com>
2020-01-14 19:53:08 +0800

    modify bug

--- calculate_ngram3.py
+++ calculate_ngram3.py
@@ -17,17 +17,18 @@ if __name__ == "__main__":
     gram_2 = {}
     with open('/Data_SSD/zhipengye/zhipengye/data/gram2/gram2_count.out', encoding='utf-8') as file:
         for line in file:
-            try:
-                segments = re.split('[\t\s]+',line)
-                first_word = segments[0]
-                second_word = segments[1]
-                count = segments[2]
-                gram_2[first_word + ' '+second_word] = count
-            except Exception as ex:
-                traceback.print_exc()
-                print(line)
-                print('segment[0]:',segments[0])
-                print('segment[1]:',segments[1])
-                print('segment[2]:',segments[2])
-                
+            if '' is not line: 
+                try:
+                    segments = re.split('[\t\s]+',line)
+                    first_word = segments[0]
+                    second_word = segments[1]
+                    count = segments[2]
+                    gram_2[first_word + ' '+second_word] = count
+                except Exception as ex:
+                    traceback.print_exc()
+                    print(line)
+                    print('segment[0]:',segments[0])
+                    print('segment[1]:',segments[1])
+                    print('segment[2]:',segments[2])
+                    
     print(gram_2.get('一 一'))
