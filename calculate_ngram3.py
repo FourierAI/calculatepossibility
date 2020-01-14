0b88d46f44627d1c6d2688fa86c007b7042a503d
FourierYe <939647181@qq.com>
2020-01-14 19:44:05 +0800

    add tracback

--- calculate_ngram3.py
+++ calculate_ngram3.py
@@ -10,16 +10,24 @@
 import os
 import re
 import main
+import traceback
 
 if __name__ == "__main__":
 
     gram_2 = {}
     with open('/Data_SSD/zhipengye/zhipengye/data/gram2/gram2_count.out', encoding='utf-8') as file:
         for line in file:
-            segments = re.split('[\s\t]+',line)
-            first_word = segments[0]
-            second_word = segments[1]
-            count = segments[2]
-            gram_2[first_word + ' '+second_word] = count
-    
+            try:
+                segments = re.split('[\t\s]+',line)
+                first_word = segments[0]
+                second_word = segments[1]
+                count = segments[2]
+                gram_2[first_word + ' '+second_word] = count
+            except Exception as ex:
+                traceback.print_exc()
+                print(line)
+                print('segment[0]：',segments[0])
+                print('segment[1]：',segments[1])
+                print('segment[2]：',segments[2])
+                
     print(gram_2.get('一 一'))
