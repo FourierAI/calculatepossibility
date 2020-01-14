bc4517f785409ae4783a0f11659bb1f76ef2b558
MrYezhipeng <939647181@qq.com>
2020-01-14 16:53:01 +0800

    ngram_3

--- count_ngram2.py
+++ count_ngram2.py
@@ -13,13 +13,13 @@ class LanguageModelContent:
 
 if __name__ == "__main__":
     
-    dir_list = sorted(os.listdir('/home/fourier/Data/data'))
+    dir_list = sorted(os.listdir('/Users/geekye/Documents/Dataset/LM/UniBiGram'))
     # because ngrams-[00030 - 00036]-of-00394 have no invalid  data
     filtered_list = [dir for dir in dir_list if dir >= 'ngrams-00001-of-00394' and dir <= 'ngrams-0029-of-00394']
 
     for file_name in filtered_list:
         grams_2 = []
-        with open('/home/fourier/Data/data/'+ file_name) as file:
+        with open('/Users/geekye/Documents/Dataset/LM/UniBiGram/'+ file_name) as file:
             for line in file:
                 if re.match('^[\u4e00-\u9fa5]{1,8}[\s\t]{1,}[\u4e00-\u9fa5]{1,8}[\s\t]{1,}\d{1,}', line):
                     segments = line.split('\t')
@@ -31,13 +31,8 @@ if __name__ == "__main__":
         if len(grams_2) == 0:
             continue
 
-        with open('/home/fourier/Data/processed_file/gram2'+ file_name, 'a') as file:
+        with open('/Users/geekye/Documents/Dataset/LM/gram2'+ file_name, 'a') as file:
             print(file_name+'has been started!')
             for model in grams_2:
                 file.write(str(model) + '\n')
             print(file_name+'has been processed!')
-
-    
-
-
-
