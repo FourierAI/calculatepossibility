ac3bf7922e609051a25f0e899723fe5545dbd19b
FourierYe <939647181@qq.com>
2020-01-14 21:09:40 +0800

    dada

--- calculate_ngram3.py
+++ calculate_ngram3.py
@@ -21,10 +21,9 @@ if __name__ == "__main__":
     gram_2 = {}
     with open('/Data_SSD/zhipengye/zhipengye/data/gram2/gram2ngrams-00002-of-00394', encoding='utf-8') as file:
         for line in file:
-            print(line)
             if '' is not line: 
                 try:
-                    segments = re.split('[\t\s]+',line)
+                    segments = re.split('[\t\s]+'.encode('utf-8'),line)
                     first_word = segments[0]
                     second_word = segments[1]
                     count = segments[2]
