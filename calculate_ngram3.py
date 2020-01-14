9a0b54509775873affb9dd746c69b0c837195aca
FourierYe <939647181@qq.com>
2020-01-14 20:34:38 +0800

    change file

--- calculate_ngram3.py
+++ calculate_ngram3.py
@@ -15,7 +15,7 @@ import traceback
 if __name__ == "__main__":
 
     gram_2 = {}
-    with open('/Data_SSD/zhipengye/zhipengye/data/gram2/gram2_count.out', encoding='gb18030') as file:
+    with open('/Data_SSD/zhipengye/zhipengye/data/gram2/gram2ngrams-00002-of-00394', encoding='utf-8') as file:
         for line in file:
             print(line)
             if '' is not line: 
