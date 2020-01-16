4cbcba21520ba0aa9c2b0aeef85ed109974cf36a
MrYezhipeng <939647181@qq.com>
2020-01-16 14:06:19 +0800

    filter file

--- filter_origindata.py
+++ filter_origindata.py
@@ -11,6 +11,13 @@ import codecs
 import sys
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
 
+
+def verifyContent(words, word_set):
+    for word in words:
+        if word not in word_set:
+            return False
+    return True
+
 if __name__ == "__main__":
 
     word_set =set()
@@ -27,8 +34,9 @@ if __name__ == "__main__":
         with open('/Data_SSD/zhipengye/zhipengye/data/original_data/Data/data/'+file_name,encoding='utf-8') as file:
             for line in file:
                 segments = line.split('\t')
-                word = segments[0].replace(' ','')
-                if word in word_set:
+                words = segments[0].split(' ')
+                flag = verifyContent(words, word_set)
+                if flag:
                     content_list.append(line.strip())
         with open('/Data_SSD/zhipengye/zhipengye/data/original_data/Data/filtered_file/'+file_name,'a',encoding='utf-8') as file:
             file.write('\n'.join(content_list))
